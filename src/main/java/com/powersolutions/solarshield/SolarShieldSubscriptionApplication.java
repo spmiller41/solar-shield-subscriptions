85b63ce3fa4a11719f3e744831b3615b5ece0f2c
Sean <spmiller41@gmail.com>
2026-04-10 15:38:53 -0400

    add test request to cmd line run

--- src/main/java/com/powersolutions/solarshield/SolarShieldSubscriptionApplication.java
+++ src/main/java/com/powersolutions/solarshield/SolarShieldSubscriptionApplication.java
@@ -1,7 +1,14 @@
 package com.powersolutions.solarshield;
 
+import com.powersolutions.solarshield.dto.SquareCheckoutResponse;
+import com.powersolutions.solarshield.repo.ContactRepo;
+import com.powersolutions.solarshield.repo.SubscriptionRepo;
+import com.powersolutions.solarshield.service.SquareSubscriptionCheckoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SolarShieldSubscriptionApplication {
@@ -11,12 +18,32 @@ public class SolarShieldSubscriptionApplication {
 	}
 
     /*
+    @Autowired
+    private SquareSubscriptionCheckoutService checkoutService;
+    private final ContactRepo contactRepo;
+    private final SubscriptionRepo subscriptionRepo;
+
+    public SolarShieldSubscriptionApplication(ContactRepo contactRepo,
+                                              SubscriptionRepo subscriptionRepo) {
+        this.contactRepo = contactRepo;
+        this.subscriptionRepo = subscriptionRepo;
+    }
+
 	@Bean
 	public CommandLineRunner demo() {
 		return (args -> {
 
+            subscriptionRepo.findById(1)
+                    .ifPresent(sub -> contactRepo.findById(1).ifPresent(contact -> {;
+
+                    SquareCheckoutResponse response = checkoutService.createSubscriptionPaymentLink(sub, contact);
+                    System.out.println(response);
+
+            }));
+
 		});
 	}
 	*/
+
 
 }
