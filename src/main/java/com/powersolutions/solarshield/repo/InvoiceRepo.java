f3c1816211377bb6a0804d942cda41951f64aa0c
Sean <spmiller41@gmail.com>
2026-04-16 14:44:15 -0400

    Add invoice repair query to link subscriptions via customer_subscription_id

--- src/main/java/com/powersolutions/solarshield/repo/InvoiceRepo.java
+++ src/main/java/com/powersolutions/solarshield/repo/InvoiceRepo.java
@@ -2,11 +2,24 @@ package com.powersolutions.solarshield.repo;
 
 import com.powersolutions.solarshield.entity.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface InvoiceRepo extends JpaRepository<Invoice, Integer> {
 
     Optional<Invoice> findByOrderId(String orderId);
+
+    @Transactional
+    @Modifying
+    @Query("""
+    UPDATE Invoice i
+       SET i.subscriptionId = :subscriptionId
+     WHERE i.customerSubscriptionId = :customerSubscriptionId
+       AND i.subscriptionId IS NULL
+    """)
+    int repairInvoicesByCustomerSubscriptionId(Integer subscriptionId, String customerSubscriptionId);
 
 }
