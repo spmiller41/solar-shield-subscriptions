dd9ea36804fe9766eb928ebf0bc3dce9891c6e2f
Sean <spmiller41@gmail.com>
2026-04-16 15:06:55 -0400

    Change argument type

--- src/main/java/com/powersolutions/solarshield/service/api/SquareEventService.java
+++ src/main/java/com/powersolutions/solarshield/service/api/SquareEventService.java
@@ -1,11 +1,13 @@
 package com.powersolutions.solarshield.service.api;
 
+import com.powersolutions.solarshield.dto.SquareInvoicePaymentRequest;
+
 import java.time.LocalDateTime;
 
 public interface SquareEventService {
 
     boolean hasProcessed(String eventId);
 
-    void recordProcessedEvent(String eventId, LocalDateTime createdAt);
+    void recordProcessedEvent(SquareInvoicePaymentRequest request);
 
 }
