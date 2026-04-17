a693187237fb7a7eb7d16dbd588dbe0c3e2f6a52
Sean <spmiller41@gmail.com>
2026-04-16 23:22:01 -0400

    Add zoho field api names

---
+++ src/main/java/com/powersolutions/solarshield/zoho/ZohoSolarShieldFields.java
@@ -0,0 +1,28 @@
+package com.powersolutions.solarshield.zoho;
+
+public final class ZohoSolarShieldFields {
+
+    private ZohoSolarShieldFields() {
+        // prevent instantiation
+    }
+
+    // Account / Core
+    public static final String NAME = "Name";
+    public static final String ACCOUNT_STATUS = "Account_Status";
+    public static final String ACTIVATED_AT = "Activated_At";
+    public static final String PLAN = "Plan";
+
+    // Contact
+    public static final String FIRST_NAME = "First_Name";
+    public static final String LAST_NAME = "Last_Name";
+    public static final String EMAIL = "Email";
+    public static final String PHONE = "Phone";
+
+    // Address
+    public static final String STREET = "Address_Street_Address";
+    public static final String CITY = "Address_City";
+    public static final String STATE = "Address_State_Province";
+    public static final String ZIP = "Address_Zip_Postal_Code";
+    public static final String COUNTRY = "Address_Country_Region";
+
+}
