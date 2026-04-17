1da2329e594295de0e551bb17bdcf27f9145e524
Sean <spmiller41@gmail.com>
2026-04-16 23:45:53 -0400

    Update commandline runner

--- src/main/java/com/powersolutions/solarshield/SolarShieldSubscriptionApplication.java
+++ src/main/java/com/powersolutions/solarshield/SolarShieldSubscriptionApplication.java
@@ -1,7 +1,9 @@
 package com.powersolutions.solarshield;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SolarShieldSubscriptionApplication {
@@ -10,33 +12,13 @@ public class SolarShieldSubscriptionApplication {
 		SpringApplication.run(SolarShieldSubscriptionApplication.class, args);
 	}
 
-    /*
-    @Autowired
-    private SquareSubscriptionCheckoutService checkoutService;
-    private final ContactRepo contactRepo;
-    private final SubscriptionRepo subscriptionRepo;
-
-    public SolarShieldSubscriptionApplication(ContactRepo contactRepo,
-                                              SubscriptionRepo subscriptionRepo) {
-        this.contactRepo = contactRepo;
-        this.subscriptionRepo = subscriptionRepo;
-    }
-
+	/*
 	@Bean
 	public CommandLineRunner demo() {
 		return (args -> {
 
-            subscriptionRepo.findById(1)
-                    .ifPresent(sub -> contactRepo.findById(1).ifPresent(contact -> {;
-
-                    SquareCheckoutResponse response = checkoutService.createSubscriptionPaymentLink(sub, contact);
-                    System.out.println(response);
-
-            }));
-
 		});
 	}
-	*/
-
+    */
 
 }
